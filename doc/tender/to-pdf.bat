243afa7e71fb4d5159a8a0c635bcd055fbd0184c
Peter Buchmann <peter.buchmann@zew.de>
2026-07-06 17:46:06 +0200

    tender 0.2

---
+++ doc/tender/to-pdf.bat
@@ -0,0 +1,18 @@
+@REM pandoc tender.md -o tender.pdf
+
+@REM pandoc tender.md -o tender.pdf -V geometry:top=18mm -V geometry:bottom=12mm -V geometry:left=30mm -V geometry:right=15mm
+
+@REM pandoc tender.md -o tender.pdf   -V geometry:top=22mm -V geometry:bottom=12mm -V geometry:left=30mm -V geometry:right=20mm  -V documentclass=extarticle   -V fontsize=12pt   -V linestretch=1.1 -V parskip=6pt
+
+pandoc tender.md -o tender.pdf ^
+  -V geometry:top=22mm ^
+  -V geometry:bottom=12mm ^
+  -V geometry:left=30mm ^
+  -V geometry:right=20mm ^
+  -V documentclass=extarticle ^
+  -V fontsize=12pt ^
+  -V linestretch=1.1 ^
+  -V parskip=6pt ^
+  -V colorlinks=true ^
+  -V urlcolor=blue ^
+  -V linkcolor=blue
